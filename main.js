4cc81ff964e4e46640117df3b458af255805e2e6
DaviSimonasse <111437630+DaviSimonasse@users.noreply.github.com>
2023-03-01 13:48:11 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,61 @@
+
+var canvas = new fabric.Canvas('myCanvas')
+ block_y=1;
+ block_x=1;
+
+var block_image_width = 350;
+var block_image_height = 430;
+
+var block_image_object= "";
+
+
+function new_image(get_image)
+{
+	fabric.Image.fromURL(get_image, function(Img) {
+		block_image_object = Img
+		block_image_object.scaleToWidth(block_image_width)
+		block_image_object.scaleToHeight(block_image_height)
+		block_image_object.set({
+			top:block_y,
+			left:block_x
+		})
+		canvas.add(block_image_object)
+	})
+}
+
+window.addEventListener("keydown", myKeyDown);
+
+function myKeyDown(e)
+{
+keyPressed = e.keyCode;
+console.log(keyPressed);
+
+	if(keyPressed == '82') 
+	{
+		new_image('rr1.png')
+		
+	}
+	if(keyPressed == '86')
+	{
+		blockX = 200;
+		new_image('gr.png')
+	}
+	
+	if(keyPressed == '65')
+	{
+		blockX =350;
+		new_image('yr.png')
+	}
+	if(keyPressed == '82')
+	{
+		blockX = 600;
+		new_image('pr.png')
+	}
+	if(keyPressed == '73')
+	{
+		blockX = 700;
+		new_image('br.png')
+	}
+	
+}
+
